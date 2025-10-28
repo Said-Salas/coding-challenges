8bd6ebf26404c375079f41f69a5e0e8a4b0830c7
Said Salas <saidsalastech@gmail.com>
2025-10-27 21:32:59 -0600

    difference of squares challenge done

--- C/difference-of-squares.c
+++ C/difference-of-squares.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main(void){
-    int numberLimit
-    printf("Up to what natural number shall I calculate?:")
+    int numberLimit;
+    printf("Up to what natural number shall I calculate?: ");
+    fflush(stdout);
+    scanf("%d", &numberLimit);
+
+    int sumOne = 0;
+    for (int i = 1; i <= numberLimit; i++){
+        sumOne += i;
+    }
+   int squareOfSum = sumOne * sumOne;
+
+   int sumTwo = 0;
+   for (int i = 1; i <= numberLimit; i++){
+        sumTwo += i * i;
+   }
+   
+   int differenceOfSums = squareOfSum - sumTwo;
+   printf("The difference of the sums is: %d", differenceOfSums);
+
+   return 0;
 }
