54f216f9c8dfdfe98f8d2bdd7bb0c0c949570506
Said Salas <saidsalastech@gmail.com>
2025-11-06 09:00:16 -0600

    progress on grains problem

---
+++ C/grains.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int grainOnSquare = 1;
+    int arrayOfGrains[64]
+    
+
+    for (int i = 0; i <= 64; i++) {
+        if (i === 0) {
+            grainOnSquare++
+        }
+        else {
+            grainOnSquare = grainOnSquare * grainOnSquare
+        }
+        i
+    }
+    return 0;
+}
+
+
+// Introduction
+// There once was a wise servant who saved the life of a prince. The king promised to pay whatever the servant could dream up. Knowing that the king loved chess, the servant told the king he would like to have grains of wheat. One grain on the first square of a chessboard, with the number of grains doubling on each successive square.
+
+// Instructions
+// Calculate the number of grains of wheat on a chessboard.
+
+// A chessboard has 64 squares. Square 1 has one grain, square 2 has two grains, square 3 has four grains, and so on, doubling each time.
+
+// Write code that calculates:
+
+// the number of grains on a given square
+// the total number of grains on the chessboard
