1a24d031f6a8765e984d897a2fc8a17427a215a5
Said Salas <saidsalastech@gmail.com>
2025-11-08 06:42:34 -0600

    progress on classic grains problem

--- C/grains.c
+++ C/grains.c
@@ -3,21 +3,17 @@
 int main(void) {
 
     int grainOnSquare = 1;
-    int arrayOfGrains[64]
-    
+    int arrayOfGrains[64];
 
-    for (int i = 0; i <= 64; i++) {
-        if (i === 0) {
-            grainOnSquare++
-        }
-        else {
-            grainOnSquare = grainOnSquare * grainOnSquare
-        }
-        i
+    for (int i = 0; i < 64; i++) {
+        grainOnSquare *= 2;
+        arrayOfGrains[i] = grainOnSquare;
+        
     }
+
+    for(int i = 0; i < 64; i++) printf("%d", arrayOfGrains[i]);
     return 0;
 }
-
 
 // Introduction
 // There once was a wise servant who saved the life of a prince. The king promised to pay whatever the servant could dream up. Knowing that the king loved chess, the servant told the king he would like to have grains of wheat. One grain on the first square of a chessboard, with the number of grains doubling on each successive square.
