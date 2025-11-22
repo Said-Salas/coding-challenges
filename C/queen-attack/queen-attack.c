8c842f89e3affa6c2917d9f85ca06ff5b471e445
Said Salas <saidsalastech@gmail.com>
2025-11-22 10:56:15 -0600

    will keep working  on queen attack's elegant solution soon

--- C/queen-attack/queen-attack.c
+++ C/queen-attack/queen-attack.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
+#include <stdio.h> //standard input/output functions
+#include <stdlib.h> //memory allocation and conversion of types functions
+#include <string.h> //string manipulation functions
+#include <ctype.h> //character functions like seeing if a char is a digit, a number or converting it to upper/lower case
+#include <stdbool.h> //we can use boolean values in our code (true/false)
 
 #define CHESSBOARD_SIZE 8 //Text substitution, the preprocessor reads it before compile time. Less error prone, easier to maintain.
 #define BUFFER_SIZE 32 
 
-int getColumnValue(char column) {
+int getColumnValue(char column) { //this switch statement is way cleaner
     switch (column) {
         case 'a': return 0;
         case 'b': return 1;
@@ -19,5 +19,9 @@ int getColumnValue(char column) {
         case 'h': return 7;
         default: return -1;
     }
+}
+
+static void strToLower(char *s) {
+    for (; *s; ++s)
 }
 
