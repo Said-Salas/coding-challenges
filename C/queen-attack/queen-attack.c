a978b7de49595a4be1f04c962cb4329d10ec2e17
Said Salas <saidsalastech@gmail.com>
2025-11-23 14:21:54 -0600

    progress with parsePosition. will continue soon

--- C/queen-attack/queen-attack.c
+++ C/queen-attack/queen-attack.c
@@ -21,7 +21,23 @@ int getColumnValue(char column) { //this switch statement is way cleaner
     }
 }
 
-static void strToLower(char *s) {
-    for (; *s; ++s)
+static void strToLower(char *s) { //'S' = 83' (ASCII value) => tolower(83) returns 115. 's' = '115'. char(115) returns 's'.
+    for (; *s; ++s) *s = (char)tolower((unsigned char)*s); // we use 'unsigned char' to make positive the ASCII value because 'tolower()' expects positive values from 0 to 255(ASCII range) and our character could or could not be signed. If signed, char would be negative and would cause bugs.
+}
+
+bool parsePosition(const char *input, int *col, int *row) {
+    if (!input) return false;
+    size_t len = strlen(input);
+    if (len != 2) return false;
+    char rawColumn = (char)tolower((unsigned char)input[0]);
+    if (!isalpha((unsigned char) rawColumn)) return false;
+    int columnValue = getColumnValue(rawColumn);
+    if (columnValue < 0) return false;
+    if (!isdigit((unsigned char)s[1])) return false;
+    int rowValue = s[1] - '1';
+    if (rowValue < 0 || rowValue >= CHESSBOARD_SIZE) return false;
+    *col = columnValue;
+    *row = rowValue;
+    return true;
 }
 
