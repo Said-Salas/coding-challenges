f07fa83a8b29379364bb4f971bffb93cff1f4826
Said Salas <saidsalastech@gmail.com>
2025-11-25 09:04:04 -0600

    nearly there

--- C/queen-attack/queen-attack.c
+++ C/queen-attack/queen-attack.c
@@ -29,12 +29,12 @@ bool parsePosition(const char *input, int *col, int *row) {
     if (!input) return false;
     size_t len = strlen(input);
     if (len != 2) return false;
-    char rawColumn = (char)tolower((unsigned char)input[0]);
+    char rawColumn = (char)tolower((unsigned char)input[0]); //input[0] is implicit dereference, so we don't write '*(input + 0).
     if (!isalpha((unsigned char) rawColumn)) return false;
     int columnValue = getColumnValue(rawColumn);
     if (columnValue < 0) return false;
-    if (!isdigit((unsigned char)s[1])) return false;
-    int rowValue = s[1] - '1';
+    if (!isdigit((unsigned char)input[1])) return false;
+    int rowValue = input[1] - '1';
     if (rowValue < 0 || rowValue >= CHESSBOARD_SIZE) return false;
     *col = columnValue;
     *row = rowValue;
@@ -42,28 +42,54 @@ bool parsePosition(const char *input, int *col, int *row) {
 }
 
 bool queenCanAttack(int columnOne, int rowOne, int columnTwo, int rowTwo) {
-    if (columnOne = columnTwo) return true;
-    if (rowOne = rowTwo) return true;
-    return abs(c1 - c2) == abs(r1 - r2); //This is the greatest enhancement. So elegant. If horizontal distance equals vertical distances, pieces are in diagonal. We use absolute value 'abs' so numbers are always positive.
+    if (columnOne == columnTwo) return true;
+    if (rowOne == rowTwo) return true;
+    return abs(columnOne - columnTwo) == abs(rowOne - rowTwo); //This is the greatest enhancement. So elegant. If horizontal distance equals vertical distances, pieces are in diagonal. We use absolute value 'abs' so numbers are always positive.
 }
 
 int main(void) {
     char posOne[BUFFER_SIZE];
     char posTwo[BUFFER_SIZE];
 
-    int col1, rowOne, columnTwo, rowTwo;
+    int colOne, rowOne, columnTwo, rowTwo;
 
     printf("Type \"exit\" to end program\n");
 
     while (1) {
         printf("Enter position of white queen: ");
-        if (!fgets(posOne, sizeof posOne, stdin)) { // Receive input on posOne and give me it's size. If size of input overflows specified size of 'posOne', break the loop and end the program.
-            fprintf(stderr, "Invalid chess position. Try again");
-            return NULL;
-        };
+        if (!fgets(posOne, sizeof posOne, stdin)) break; // Receive input on posOne and give me it's size. If size of input overflows specified size of 'posOne' --defined by 'BUFFER_SIZE'-- trim it and keep what can be fitted. For instance, entering a 50 byte string will cause 'fgets' to only store up to 31 characters + null terminator. 1 character = 1 byte(8 bits).
+        //However, 'fgets' returns 'False' when the input stream is closed/dead. Pressing Ctrl+D (Mac) or Ctrl+Z (Windows) closes the pipe feeding data to the program and would make the expression falsy, exiting loop and ending the program.
         posOne[strcspn(posOne, "\r\n")] == '\0'; //replace any Enter (\n) with null terminator (\0) by looping through each character of the string array and checking.
         char copyPosOne[BUFFER_SIZE]; //BUFFER_SIZE here => Reserve 32 bytes of memory for this array
         strncpy(copyPosOne, posOne, BUFFER_SIZE); //BUFFER_SIZE here => stop copying after 32 characters, even if the source is longer (should not happen).
-        
+        copyPosOne[BUFFER_SIZE - 1] = '\0';
+        strToLower(copyPosOne);
+        if (strcmp(tmp1, "exit") == 0) break;
+
+        printf("Enter position of black queen: ");
+        if (!fgets(posTwo, sizeof posTwo, stdin)) break;
+        posTwo[strcspn(posTwo, "\r\n")] = '\0';
+        char copyPosTwo[BUFFER_SIZE];
+        strncpy(copyPosTwo, posTwo, BUFFER_SIZE);
+        copyPosTwo[BUFFER_SIZE - 1] = '\0';
+        strToLower(copyPosTwo);
+        if(strcmp(copyPosTwo, "exit") == 0) break;
+
+        if (!parsePosition(copyPosOne, &colOne, &colTwo)) {
+            fprintf(stderr, "Please enter a valid chess position for the white queen.\n");
+            continue; //we don't exit loop or end program, rather we skip the current iteration and go back to the top of loop again. This will let user start 'fresh' and enter input again.
+        }
+        if (!parsePosition(copyPosTwo, &columnTwo, &rowTwo)) {
+            fprintf(stderr, "Please enter a valid chess position for the black queen.\n");
+            continue;
+        }
+
+        if (queenCanAttack(colOne, rowOne, columnTwo, rowTwo)) {
+            printf("Queens can attack each other\n");
+        } else {
+            printf("Queens can't attack each other\n");
+        }
     }
+
+    return 0;
 }
